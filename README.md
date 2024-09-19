2cc3c927cf3f43224fdaa7b224bf95c1b917a099
ka-mahmood <kashfia135@gmail.com>
2024-09-18 23:29:08 -0400

    Made more concise - expanded on README, compared to proper.

--- README.md
+++ README.md
@@ -2,15 +2,16 @@ Ash Mahmood
 
 BME1478 Assignment 2 script & files are in this repository. All of the task scripts are run using the following, with the terminal in this directory:
 
-`bash assignment2_script.sh`
+`bash a2_run_subscripts.sh` or `bash a2_full_script.sh`. The former runs each subscript, while the latter contains all the code combined into one for easier submission.
 
 The individual scripts for each task, up to 5, can be run using:
 
-`bash task{#}.sh`
+`bash task#.sh`
 
 E.g., Task 1 is run with `bash task1.sh`.
 
-The files currently in the directory are shown post-processing. Replace `./assignment-data` with the unprocessed information and re-run the script to test. 
+The files currently in the directory are shown post-processing. Replace `assignment-data` with the unprocessed information (`assignment-data-original`) and re-run the script to test. 
 
-The `assignment-data-copy` folder contains the original data. This is used for the comparison using `diff` for task 4. The explanation for using `diff` is included in the script output.
-The `assignment-data-proper` folder contains the manually edited data to compare the post-processed data to.
+The `assignment-data-original` folder contains the original data. This is used to overwrite the revised files made through running the command using `diff` for task 4. The explanation for using `diff` is included in the script output.
+
+The `assignment-data-proper` folder contains the manually edited data to compare the post-processed data to. This is what is used for comparison in Task 4.
--- task4.sh
+++ task4.sh
@@ -3,20 +3,20 @@
 # TASK 4
 
 echo -------------------- PART FOUR --------------------
-echo Checking that assignment-data copy is intact. As per TA suggestion, including file and folder comparison:
+echo Checking that assignment-data is intact. As per TA suggestion, including file and folder comparison:
 # echo To compare folders, you can use the "diff" command. 
 # echo This is used in the form "diff {option} ... {path 1} {path 2}". \
 	# It compares the contents of the path - if given a directory path, it compares \
-	# to the first depth by default: "diff assignment-data assignment-data-copy" will only \
+	# to the first depth by default: "diff assignment-data assignment-data-proper" will only \
 	# compare the names of folders under assignment-data. If any of the files within the \
 	# folders are missing, then "diff" will not output a difference. The file names will \
 	# have to be changed to those that are of the hard drive folder and the main computer. 
 
 # echo Some of the options include "-r" for recursive, "-i" to ignore case, and "-y" to show \
 	# a side-by-side comparison. The following is the output of \
-	# "diff -r assignment-data assignment-data-copy:", which uses the code 'r' to recursively also \
+	# "diff -r assignment-data assignment-data-proper:", which uses the code 'r' to recursively also \
 	# check the files that are missing.
 
-if diff -r assignment-data assignment-data-copy; then
-	echo "All files and folder names are the same."
+if diff -r assignment-data assignment-data-proper; then
+	echo "All file and folder names are the same."
 fi
